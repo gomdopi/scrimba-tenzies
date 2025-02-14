20c70826a0553026b6f12f6f99faf2a3371fe66b
gomdopi <io.gomdopi@gmail.com>
2025-02-14 09:29:45 -0800

    tenzies game done all by myself

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import { useState } from "react"
-import Header from "./components/Header"
+import { useEffect, useState } from "react"
 import DiceContainer from "./components/DiceContainer"
+import Header from "./components/Header"
 
 export type Dice = {
   idx: number
@@ -9,7 +9,14 @@ export type Dice = {
 }
 
 export default function App() {
-  function initializeDie() {
+  const [die, setDie] = useState(initialDie())
+  const [hasWon, setHasWon] = useState(false)
+
+  useEffect(() => {
+    checkHasWon()
+  }, [die])
+
+  function initialDie(): Array<Dice> {
     const die: Array<Dice> = []
 
     for (let i = 0; i < 10; i++) {
@@ -24,26 +31,55 @@ export default function App() {
     return die
   }
 
-  function handleDiceClick(idx: number) {
+  function resetDie(): void {
+    setDie(initialDie())
+  }
+
+  function rollDie(): void {
+    setDie(prevDie =>
+      prevDie.map(dice => (dice.frozen ? dice : rollDice(dice)))
+    )
+  }
+
+  function rollDice(dice: Dice): Dice {
+    const value = Math.floor(Math.random() * 6) + 1
+    return {
+      ...dice,
+      value,
+    }
+  }
+
+  function checkHasWon(): void {
+    const value = die[0].value
+    if (die.every(dice => dice.value === value && dice.frozen)) {
+      setHasWon(true)
+    }
+  }
+
+  function handleDiceClick(idx: number): void {
+    if (hasWon) return
     setDie(prevDie => {
       return prevDie.map(dice =>
         dice.idx === idx ? { ...dice, frozen: !dice.frozen } : dice
       )
     })
-    console.log(idx)
   }
 
-  function handleClick() {
-    setDie(initializeDie())
+  function handleClick(): void {
+    if (hasWon) {
+      resetDie()
+      setHasWon(false)
+    } else {
+      rollDie()
+    }
   }
 
-  const [die, setDie] = useState(initializeDie())
   return (
     <main>
       <Header statusText="Roll until all dice are the same. Click each die to freeze it at its current value between rolls." />
       <DiceContainer die={die} onDiceClick={handleDiceClick} />
       <button className="game-button" onClick={handleClick}>
-        Roll
+        {hasWon ? "New Game" : "Roll"}
       </button>
     </main>
   )
